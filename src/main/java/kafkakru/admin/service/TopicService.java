567ec76e574927d99305082007bf056b5a06a9a7
jinhwan2 <dilcheck@naver.com>
2022-10-31 18:01:54 +0900

    토픽 CRD 추가 (#6)
    Co-authored-by: lee.jinhwan <lee.jinhwan@navercorp.com>

--- src/main/java/kafkakru/admin/service/TopicService.java
+++ src/main/java/kafkakru/admin/service/TopicService.java
@@ -1,20 +1,34 @@
 package kafkakru.admin.service;
 
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.TopicDescription;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
+import kafkakru.admin.dto.CreateTopicRequest;
 import kafkakru.admin.dto.Topic;
 
 @Service
 @RequiredArgsConstructor
 public class TopicService {
     private final AdminClient kafkaAdminClient;
+
+    public boolean createTopic(CreateTopicRequest createTopicRequest) throws ExecutionException, InterruptedException {
+        NewTopic newTopic = new NewTopic(createTopicRequest.getTopicName(), createTopicRequest.getNumPartitions(), createTopicRequest.getReplicationFactor().shortValue());
+
+        this.kafkaAdminClient.createTopics(List.of(newTopic))
+            .all()
+            .get();
+
+        return true;
+    }
 
     public Set<Topic> getTopics() throws ExecutionException, InterruptedException {
         return this.kafkaAdminClient.listTopics()
@@ -23,5 +37,22 @@ public class TopicService {
                 .map(topic -> new Topic(topic.name(), topic.topicId().toString()))
                 .collect(Collectors.toSet())
             ).get();
+    }
+
+    public Topic getTopic(String topicName) throws ExecutionException, InterruptedException {
+        return this.kafkaAdminClient.describeTopics(List.of(topicName))
+            .allTopicNames()
+            .thenApply(it -> {
+                TopicDescription topic = it.get(topicName);
+                return new Topic(topic.name(), topic.topicId().toString());
+            }).get();
+    }
+
+    public boolean deleteTopic(String topicName) throws ExecutionException, InterruptedException {
+        this.kafkaAdminClient.deleteTopics(List.of(topicName))
+            .all()
+            .get();
+
+        return true;
     }
 }
