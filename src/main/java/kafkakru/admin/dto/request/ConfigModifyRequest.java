d676e83d17b5c2dccc4095e1fa2d4f25951df796
lee.jinhwan <lee.jinhwan@navercorp.com>
2022-11-01 10:54:46 +0900

    사용안하는 annotation 제거

--- src/main/java/kafkakru/admin/dto/request/ConfigModifyRequest.java
+++ src/main/java/kafkakru/admin/dto/request/ConfigModifyRequest.java
@@ -2,13 +2,11 @@ package kafkakru.admin.dto.request;
 
 import java.util.Map;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class ConfigModifyRequest {
     @NonNull
